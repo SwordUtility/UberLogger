3d377e5d3282cf693518f76a6272c76bd4be729e
yangzijian <zijian_yang@yeah.net>
2024-10-09 14:14:33 +0800

    同步log数据

--- Examples/TestUberLogger.cs
+++ Examples/TestUberLogger.cs
@@ -1,82 +1,121 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 using System.Threading;
+using System;
 
-public class TestUberLogger : MonoBehaviour
+namespace HsufAOT
 {
-    Thread TestThread;
-    // Use this for initialization
-    void Start ()
-    {
-        UberLogger.Logger.AddLogger(new UberLoggerFile("UberLogger.log"), false);
-        DoTest();
-        TestThread = new Thread(new ThreadStart(TestThreadEntry));
-        TestThread.Start();
 
-        //Test an internal .Net OOB error
-        var t = new List<int>();
-        t[0] = 5;
-    }
 
-    void OnDestroy()
+    public class TestUberLogger : MonoBehaviour
     {
-        TestThread.Abort();
-        TestThread.Join();
-    }
-    void TestThreadEntry()
-    {
-        for(;;)
+        Thread TestThread;
+        // Use this for initialization
+        void Start()
         {
-            Debug.Log("Thread Log Message");
-            UberDebug.Log("Thread ULog Message");
-            Thread.Sleep(100);
+            UberLogger.Logger.AddLogger(new UberLoggerFile("UberLogger.log"), false);
+            DoTest();
+            TestThread = new Thread(new ThreadStart(TestThreadEntry));
+            TestThread.Start();
+
+            //Test an internal .Net OOB error
+            var t = new List<int>();
+            t[0] = 5;
+        }
+
+        void OnDestroy()
+        {
+            if (TestThread != null && TestThread.IsAlive)
+            {
+                TestThread.Abort();
+                TestThread.Join();
+            }
+        }
+        void TestThreadEntry()
+        {
+            for (int id = 0; id < 10000; id++)
+            {
+                Debug.Log("Thread Log Message");
+                Log.Info("Thread ULog Message");
+                Thread.Sleep(0);
+            }
+            while (true)
+            {
+                Debug.Log("Thread Log Message");
+                Log.Info("Thread ULog Message");
+                Thread.Sleep(1000);
+            }
+        }
+
+        public void DoTest()
+        {
+            // UnityEngine.Debug.Log("Starting");
+            Debug.LogWarning("Log Warning with GameObject", gameObject);
+            Debug.LogError("Log Error with GameObject", gameObject);
+            Debug.Log("Log Message with GameObject", gameObject);
+            Debug.LogFormat("Log Format param {0}", "Test");
+            Debug.LogFormat(gameObject, "Log Format with GameObject and param {0}", "Test");
+
+            Log.Info("ULog");
+            Log.Info("ULog with param {0}", "Test");
+            Log.Info(gameObject, "ULog with GameObject");
+            Log.Info(gameObject, "ULog with GameObject and param {0}", "Test");
+
+            Log.InfoChannel("Test", "ULogChannel");
+            Log.InfoChannel("Test", "ULogChannel with param {0}", "Test");
+            Log.InfoChannel(gameObject, "Test", "ULogChannel with GameObject");
+            Log.InfoChannel(gameObject, "Test", "ULogChannel with GameObject and param {0}", "Test");
+
+            Log.Warning("ULogWarning");
+            Log.Warning("ULogWarning with param {0}", "Test");
+            Log.Warning(gameObject, "ULogWarning with GameObject");
+            Log.Warning(gameObject, "ULogWarning with GameObject and param {0}", "Test");
+
+            Log.WarningChannel("Test", "ULogWarningChannel");
+            Log.WarningChannel("Test", "ULogWarningChannel with param {0}", "Test");
+            Log.WarningChannel(gameObject, "Test", "ULogWarningChannel with GameObject");
+            Log.WarningChannel(gameObject, "Test", "ULogWarningChannel with GameObject and param {0}", "Test");
+
+            Log.Error("ULogError");
+            Log.Error("ULogError with param {0}", "Test");
+            Log.Error(gameObject, "ULogError with GameObject");
+            Log.Error(gameObject, "ULogError with GameObject and param {0}", "Test");
+
+            Log.ErrorChannel("Test", "ULogErrorChannel");
+            Log.ErrorChannel("Test", "ULogErrorChannel with param {0}", "Test");
+            Log.ErrorChannel(gameObject, "Test", "ULogErrorChannel with GameObject");
+            Log.ErrorChannel(gameObject, "Test", "ULogErrorChannel with GameObject and param {0}", "Test");
+
+            // try
+            // {
+            //     Log.Fatal("ULogFatal", new System.Exception("Test Exception"));
+            // }
+            // catch (Exception e)
+            // {
+            //     Log.InfoChannel("Test", $"exception: {e.Message} stacktrace: {e.StackTrace}");
+            //     Debug.LogException(e);
+            //     Log.Info($"exception string:{e.ToString()}");
+            //     Log.Info($"exception {e}");
+            //     Log.Fatal("adfadfa ULogFatal", e);
+            // }
+
+            try{
+                throw new System.Exception("adfadf Exception111");
+            }
+            catch(Exception e)
+            {
+                Log.Fatal("adaaaaaa ULogFatal", e);
+            }
+
+            // Log.Fatal("ULogFatal with GameObject", new System.Exception("Test Exception22"));
+            // throw new System.Exception("Test Exception33");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // DoTest();
         }
     }
 
-    public void DoTest()
-    {
-        // UnityEngine.Debug.Log("Starting");
-        Debug.LogWarning("Log Warning with GameObject", gameObject);
-        Debug.LogError("Log Error with GameObject", gameObject);
-        Debug.Log("Log Message with GameObject", gameObject);
-        Debug.LogFormat("Log Format param {0}", "Test");
-        Debug.LogFormat(gameObject, "Log Format with GameObject and param {0}", "Test");
-
-        UberDebug.Log("ULog");
-        UberDebug.Log("ULog with param {0}", "Test");
-        UberDebug.Log(gameObject, "ULog with GameObject");
-        UberDebug.Log(gameObject, "ULog with GameObject and param {0}", "Test");
-
-        UberDebug.LogChannel("Test", "ULogChannel");
-        UberDebug.LogChannel("Test", "ULogChannel with param {0}", "Test");
-        UberDebug.LogChannel(gameObject, "Test", "ULogChannel with GameObject");
-        UberDebug.LogChannel(gameObject, "Test", "ULogChannel with GameObject and param {0}", "Test");
-	
-        UberDebug.LogWarning("ULogWarning");
-        UberDebug.LogWarning("ULogWarning with param {0}", "Test");
-        UberDebug.LogWarning(gameObject, "ULogWarning with GameObject");
-        UberDebug.LogWarning(gameObject, "ULogWarning with GameObject and param {0}", "Test");
-
-        UberDebug.LogWarningChannel("Test", "ULogWarningChannel");
-        UberDebug.LogWarningChannel("Test", "ULogWarningChannel with param {0}", "Test");
-        UberDebug.LogWarningChannel(gameObject, "Test", "ULogWarningChannel with GameObject");
-        UberDebug.LogWarningChannel(gameObject, "Test", "ULogWarningChannel with GameObject and param {0}", "Test");
-
-        UberDebug.LogError("ULogError");
-        UberDebug.LogError("ULogError with param {0}", "Test");
-        UberDebug.LogError(gameObject, "ULogError with GameObject");
-        UberDebug.LogError(gameObject, "ULogError with GameObject and param {0}", "Test");
-
-        UberDebug.LogErrorChannel("Test", "ULogErrorChannel");
-        UberDebug.LogErrorChannel("Test", "ULogErrorChannel with param {0}", "Test");
-        UberDebug.LogErrorChannel(gameObject, "Test", "ULogErrorChannel with GameObject");
-        UberDebug.LogErrorChannel(gameObject, "Test", "ULogErrorChannel with GameObject and param {0}", "Test");
-    }
-	
-	// Update is called once per frame
-    void Update ()
-    {
-        // DoTest();
-    }
 }
-
--- UberLoggerFile.cs
+++ UberLoggerFile.cs
@@ -1,41 +1,72 @@
-using UberLogger;
 using System.IO;
 using UnityEngine;
+using System;
 
 /// <summary>
 /// A basic file logger backend
 /// </summary>
-public class UberLoggerFile : UberLogger.ILogger
+namespace HsufAOT
 {
-    private StreamWriter LogFileWriter;
-    private bool IncludeCallStacks;
-
-    /// <summary>
-    /// Constructor. Make sure to add it to UberLogger via Logger.AddLogger();
-    /// filename is relative to Application.persistentDataPath
-    /// if includeCallStacks is true it will dump out the full callstack for all logs, at the expense of big log files.
-    /// </summary>
-    public UberLoggerFile(string filename, bool includeCallStacks = true)
+    using UberLogger;
+    public class UberLoggerFile : UberLogger.ILogger
     {
-        IncludeCallStacks = includeCallStacks;
-        var fileLogPath = System.IO.Path.Combine(Application.persistentDataPath, filename);
-        Debug.Log("Initialising file logging to " + fileLogPath);
-        LogFileWriter = new StreamWriter(fileLogPath, false);
-        LogFileWriter.AutoFlush = true;
-    }
+        private StreamWriter LogFileWriter;
+        private bool IncludeCallStacks;
 
-    public void Log(LogInfo logInfo)
-    {
-        lock(this)
+        public static string LogFilePath { get; private set; }
+
+        /// <summary>
+        /// Constructor. Make sure to add it to UberLogger via Logger.AddLogger();
+        /// filename is relative to Application.persistentDataPath
+        /// if includeCallStacks is true it will dump out the full callstack for all logs, at the expense of big log files.
+        /// </summary>
+        public UberLoggerFile(string filename, bool includeCallStacks = true)
         {
-            LogFileWriter.WriteLine(logInfo.Message);
-            if(IncludeCallStacks && logInfo.Callstack.Count>0)
+            IncludeCallStacks = includeCallStacks;
+#if UNITY_EDITOR
+            filename = System.IO.Path.Combine("../../Logs", DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss") + "_" + filename.ToString());
+            var fileLogPath = HsufFileUtility.GetModePath(filename, HsufFileMode.Editor);
+#else
+            var fileLogPath = HsufFileUtility.GetModePath(filename, HsufFileMode.Runtime_SerializedData);
+#endif
+            LogFilePath = fileLogPath;
+            string folder = System.IO.Path.GetDirectoryName(fileLogPath);
+            if (!System.IO.Directory.Exists(folder))
             {
-                foreach(var frame in logInfo.Callstack)
+                System.IO.Directory.CreateDirectory(folder);
+            }
+            Debug.Log("Initialising file logging to " + fileLogPath);
+            LogFileWriter = new StreamWriter(fileLogPath, false);
+            LogFileWriter.AutoFlush = true;
+        }
+
+        public void Log(LogInfo logInfo)
+        {
+            lock (this)
+            {
+                string title = "";
+                switch (logInfo.Severity)
                 {
-                    LogFileWriter.WriteLine(frame.GetFormattedMethodNameWithFileName());
+                    case LogSeverity.Error:
+                        title = "[ERROR]";
+                        break;
+                    case LogSeverity.Warning:
+                        title = "[WARNING]";
+                        break;
+                    case LogSeverity.Message:
+                        title = "[INFO]";
+                        break;
                 }
-                LogFileWriter.WriteLine();
+                LogFileWriter.WriteLine($"[{AOTTextUtility.GetLocalTime()}][{title}][{logInfo.Channel}]{logInfo.Message}");
+                // Only include callstacks for errors and warnings, or if explicitly requested
+                if ((IncludeCallStacks || logInfo.Severity >= LogSeverity.Warning) && logInfo.Callstack.Count > 0)
+                {
+                    foreach (var frame in logInfo.Callstack)
+                    {
+                        LogFileWriter.WriteLine(frame.GetFormattedMethodNameWithFileName());
+                    }
+                    LogFileWriter.WriteLine();
+                }
             }
         }
     }
